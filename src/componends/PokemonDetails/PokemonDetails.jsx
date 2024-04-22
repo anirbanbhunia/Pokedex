884c1c9c66e2f852d715e0b86aafb36553c356f1
anirbanbhunia <anirbanbhunia7@gmail.com>
2024-04-22 20:57:57 +0530

    implement styling

--- src/componends/PokemonDetails/PokemonDetails.jsx
+++ src/componends/PokemonDetails/PokemonDetails.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { MdCatchingPokemon } from "react-icons/md"
+import './PokemonDetails.css'
 
 function PokemonDetails({pokeName}){
     const {id} = useParams()
     const [pokemon,setPokemon] = useState({})
+    const[wrong,setWrong] = useState(false)
+    const[isLoding,setIsLoading] = useState(true)
     async function downloadPokemon(){
         try{
+            setIsLoading(true)
             let res
             let res2
             if(pokeName){
@@ -15,16 +20,20 @@ function PokemonDetails({pokeName}){
                 res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
                 res2 = await res.json()
             }
-            //console.log(res2)
+            //console.log("res2 is:",res2)
             setPokemon({
                 name: res2.name,
                 height: res2.height,
                 weight: res2.weight,
                 image: (res2.sprites.other.dream_world.front_default) ? res2.sprites.other.dream_world.front_default : (res2.sprites.other.home.front_default)? res2.sprites.other.home.front_default : res2.sprites.front_shiny,
-                types: res2.types.map((p) => p.type.name)
+                types: res2.types.map((p) => p.type.name),
+                id1: res2.id
             })
+            setWrong(false)
+            setIsLoading(false)
         }catch{
-            console.log("wrong")
+            setWrong(true)
+            setIsLoading(false)
         }
     }
     useEffect(() =>{
@@ -32,14 +41,74 @@ function PokemonDetails({pokeName}){
         // eslint-disable-next-line
     },[])
     return(
-        <div>
-            <p>{pokemon.name}</p>
-            <img src={pokemon.image} alt="pokemon"/>
-            <div>
-                {pokemon.types && pokemon.types.map((x) => <p key={x}>{x}</p>)} {/* if pokemon.types exist only that time it will render */}
+        <div className="flex flex-col h-screen items-center w-full">
+        {(!isLoding)?!wrong?
+            <div className="flex flex-col mt-10 w-full items-center">
+                <div className="w-[380px] p-2 bg-yellow-300 rounded-full transition duration-200 hover:shadow-lg hover:shadow-gray-500 hover:-translate-y-3">
+                        <h1 className="text-center font-serif font-semibold text-red-800 text-3xl"># {pokemon.id1}</h1>
+                </div>
+                <div className="flex w-full justify-around mt-4">
+                    <div>
+                        <div>
+                            {pokemon.types?<span>Type</span>:null}
+                        </div>
+                        <div className="flex">
+                            {pokemon.types && pokemon.types.map((x) =>
+                            <div>
+                                <p key={x}>{x}</p>
+                            </div>)} {/* if pokemon.types exist only that time it will render */}
+                        </div>
+                    </div>
+                    <div className="bg-gray-400 rounded-lg w-[380px] h-[420px] flex justify-center items-center p-8 transition duration-200 hover:shadow-xl hover:shadow-gray-500 hover:bg-gray-300 hover:-translate-y-3">
+                        <img id="pokeImg" src={pokemon.image} alt="" className=" min-w-[480px]
+                         max-h-[480px]"/>
+                    </div>
+                    <div className="flex">
+                        <div>
+                            <div>
+                                <p>HEIGHT</p>
+                            </div>
+                            <div>
+                                <p>{(pokemon.weight)/10} kg</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <p>WEIGHT</p>
+                            </div> 
+                            <div>
+                                <p>{(pokemon.height)/10} m</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-[380px] p-2 bg-yellow-300 rounded-full transition duration-200 hover:shadow-lg hover:shadow-gray-500 mt-6">
+                    <p className="text-center mb-2 tracking-[8px] font-light">{pokemon.name.toUpperCase()}</p>
+                </div>
             </div>
-            <p>{pokemon.weight}</p>
-            <p>{pokemon.height}</p>
+            :
+            <div className="flex flex-col h-screen items-center mt-24">
+                <div>
+                    <h3 className="font-semibold font-sans tracking-[8px] text-orange-700 text-2xl text-center">SORRY</h3>
+                </div>
+                <div className="flex relative right-3">
+                    <h1 className="text-[100px] font-medium text-black/70">4</h1>
+                    <MdCatchingPokemon id="logoPoke" className="text-[75px] animate-pulse text-red-400 mt-9"/>
+                    <h1 className="text-[100px] font-medium text-black/70">4</h1>
+                </div>
+                <div className=" bg-transparent w-[200px] h-[2px] relative bottom-4 right-2 rounded-full border-none" id="shadow"></div>
+                <div>
+                    <p className="text-center mt-5 mb-2 tracking-[6px] font-medium relative right-1 text-orange-900">POKEMON &nbsp;NOT &nbsp;FOUND</p>
+                </div>
+            </div>
+            :
+            <div className="flex flex-col w-full items-center justify-center mt-8 mb-8">
+                <div className="flex items-center gap-2">
+                            <MdCatchingPokemon className="animate-spin font-thin text-2xl text-gray-400" />
+                        <span className="text-lg text-gray-400">Loading....</span>
+                </div>
+            </div>
+            }
         </div>
     )
 }
--- src/componends/SearchBox/SearchBox.jsx
+++ src/componends/SearchBox/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({update}){
     return(
         <div>
             <input
-                type="text" placeholder="pokemon name...."
+                type="text" placeholder="pokemon name or number...."
                 className="mt-8 w-[350px] p-2 outline-none border-gray-400 border-2 sm:w-[500px] sm:p-3 sm:mt-10 rounded-[4px]"
                 onChange={debounce}
             />
